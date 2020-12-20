68c1e25a62071db1260efbaf237842a18146665c
cooliohus <andy@coolioh.com>
2020-12-19 21:29:57 -0500

    Add config.h file

---
+++ iot-freezer/include/config.h
@@ -0,0 +1,45 @@
+/************************ Adafruit IO Config *******************************/
+
+// visit io.adafruit.com if you need to create an account,
+// or if you need your Adafruit IO key.
+#define IO_USERNAME    "cooliohus"
+#define IO_KEY         "9539c772b0d44744b3bbfd712fd92213"
+
+/******************************* WIFI **************************************/
+
+// the AdafruitIO_WiFi client will work with the following boards:
+//   - HUZZAH ESP8266 Breakout -> https://www.adafruit.com/products/2471
+//   - Feather HUZZAH ESP8266 -> https://www.adafruit.com/products/2821
+//   - Feather M0 WiFi -> https://www.adafruit.com/products/3010
+//   - Feather WICED -> https://www.adafruit.com/products/3056
+
+#define WIFI_SSID       "WAC-ATC"
+#define WIFI_PASS       "zero2+Six"
+
+// comment out the following two lines if you are using fona or ethernethttps://duckduckgo.com/?q=adafruit_sensor.h&t=lm&atb=v146-1&ia=software
+#include "AdafruitIO_WiFi.h"
+//#include "/home/acooper/sketchbook/libraries/Adafruit_IO_Arduino/src/AdafruitIO_WiFi.h"
+
+AdafruitIO_WiFi iox(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+
+
+/******************************* FONA **************************************/
+
+// the AdafruitIO_FONA client will work with the following boards:
+//   - Feather 32u4 FONA -> https://www.adafruit.com/product/3027
+
+// uncomment the following two lines for 32u4 FONA,
+// and comment out the AdafruitIO_WiFi client in the WIFI section
+// #include "AdafruitIO_FONA.h"
+// AdafruitIO_FONA io(IO_USERNAME, IO_KEY);
+
+
+/**************************** ETHERNET ************************************/
+
+// the AdafruitIO_Ethernet client will work with the following boards:
+//   - Ethernet FeatherWing -> https://www.adafruit.com/products/3201
+
+// uncomment the following two lines for ethernet,
+// and comment out the AdafruitIO_WiFi client in the WIFI section
+// #include "AdafruitIO_Ethernet.h"
+// AdafruitIO_Ethernet io(IO_USERNAME, IO_KEY);
