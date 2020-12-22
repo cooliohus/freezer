95b93cb5710b0c032c9a67ec91a8f13837771273
cooliohus <andy@coolioh.com>
2020-12-22 08:10:46 -0500

    update

--- iot-freezer/src/main.cpp
+++ iot-freezer/src/main.cpp
@@ -31,15 +31,8 @@
 // or ethernet clients.
 //
 #include "config.h"   // configuration stuff
-
 #include <OneWire.h>
 #include <DallasTemperature.h>
-
-//#include <Wire.h>
-//#include <SPI.h>
-//#include <Adafruit_Sensor.h>
-//#include <Adafruit_BME280.h>
-//#include <DHT.h>
 
 // defines for temp / humidity sensor
 //#define DHTPIN 12
@@ -51,6 +44,7 @@ unsigned long maxDelay = 26843542;
 #define oneSecond 1000000 / oneTick    // one second (1000000 uS / tick)
 #define IODELAY 15                     // generate interrupt every 15 seconds
 
+
 unsigned long counter = 0;
 unsigned long ftempcnt = 0;
 unsigned long fhumcnt = 0;
@@ -59,8 +53,8 @@ float vout;
 
 
 boolean state = false;
-#define high_on 1.0
-#define low_off -3.0
+#define high_on 32.0
+#define low_off 25.0
 
 unsigned long mydelay;
 
@@ -71,12 +65,14 @@ volatile int intCnt = 0;
 volatile bool gotReply = true;
 
 // set up the adafruitio feeds.  Note that the object iox was created in config.h
-AdafruitIO_Feed *ffreezer = iox.feed("freezertemp");
-// AdafruitIO_Feed *fouttemp = iox.feed("outlettemp");
+AdafruitIO_Feed *feedambient = iox.feed("ambienttemp");
+AdafruitIO_Feed *feedfreezer = iox.feed("freezertemp");
 
 // Data wire is plugged into port 2 on the Arduino
 #define ONE_WIRE_BUS 2
 #define TEMPERATURE_PRECISION 9
+
+#define ON_LED 16
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
@@ -85,18 +81,18 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 // arrays to hold device addresses
-DeviceAddress tfreezer;
+DeviceAddress devambient = { 0x28, 0xFF, 0x1D, 0xCE, 0x39, 0x20, 0x01, 0x3B };
+//DeviceAddress devfreezer = { 0x28, 0xC6, 0xB0, 0xD5, 0x39, 0x20, 0x01, 0x33 };
+DeviceAddress devfreezer = { 0x28, 0xD8, 0x19, 0xCD, 0x39, 0x20, 0x01, 0x9F };
 
-// Assign address manually. The addresses below will need to be changed
-// to valid device addresses on your bus. Device address can be retrieved
-// by using either oneWire.search(deviceAddress) or individually via
-// sensors.getAddress(deviceAddress, index)
-// DeviceAddress insideThermometer = { 0x28, 0x1D, 0x39, 0x31, 0x2, 0x0, 0x0, 0xF0 };
-// DeviceAddress outsideThermometer   = { 0x28, 0x3F, 0x1C, 0x31, 0x2, 0x0, 0x0, 0x2 };
+//inx0 0 Address: 28C6B0D539200133
+//inx1 0 Address: 28FF1DCE3920013B
+
+//  Freezer sensor = 28 D8 19 CD 39 20 01 9F
+
 
 void printTemperature(DeviceAddress deviceAddress);
 void printAddress(DeviceAddress deviceAddress);
-
 
 
 // This function is called each time the interval timer times out
@@ -125,6 +121,11 @@ void handleMessage(AdafruitIO_Data *data) {
     Serial.print("ack from freezertemp: ");
     Serial.println(data->toChar());
   }
+  else if (strcmp((char *)data->feedName(),"ambienttemp")== 0) {
+    ftempcnt += 1;
+    Serial.print("ack from ambienttemp: ");
+    Serial.println(data->toChar());
+  }
   else {
     Serial.print("unknown message from: ");
     Serial.print(data->feedName()); Serial.print("  ");
@@ -138,18 +139,19 @@ void setup() {
 
   // start the serial connection
   Serial.begin(115200);
-  delay(500);
   
   // wait for serial monitor to open
   while(! Serial);
     delay(100);
 
+  delay(1000);  // wait for serial port to fully initialize
+  
   // connect to io.adafruit.com
   Serial.print("Connecting to Adafruit IO");
-
   iox.connect();   // connect and sign in to adaio
 
   // wait for a connection
+  
   while(iox.status() < AIO_CONNECTED) {
     delay(500);
     Serial.print("."); 
@@ -171,10 +173,10 @@ void setup() {
   
   // setup message handlers - all messages received from ada io are directed to the handleMessage function
   // except handle lights because we want to do something - it is easier to process in a separate function
-  ffreezer->onMessage(handleMessage);
-  //fouttemp->onMessage(handleMessage);
+  feedfreezer->onMessage(handleMessage);
+  feedambient->onMessage(handleMessage);
   
- Serial.println("Dallas Temperature IC Control Library Demo");
+  Serial.println("Dallas Temperature IC Control Library Demo");
 
   // Start up the library
   sensors.begin();
@@ -190,65 +192,50 @@ void setup() {
   if (sensors.isParasitePowerMode()) Serial.println("ON");
   else Serial.println("OFF");
 
-  // Search for devices on the bus and assign based on an index. Ideally,
-  // you would do this to initially discover addresses on the bus and then
-  // use those addresses and manually assign them (see above) once you know
-  // the devices on your bus (and assuming they don't change).
-  //
-  // method 1: by index
-  if (!sensors.getAddress(tfreezer, 1)) Serial.println("Unable to find address for Device 0");
-  //if (!sensors.getAddress(outsideThermometer, 0)) Serial.println("Unable to find address for Device 1");
+    // show the addresses we found on the bus
+  DeviceAddress inx0, inx1;
+  if (!sensors.getAddress(inx0, 0)) Serial.println("Unable to find address for Device inx0");
+  if (!sensors.getAddress(inx1, 1)) Serial.println("Unable to find address for Device inx1");
 
-  // method 2: search()
-  // search() looks for the next device. Returns 1 if a new address has been
-  // returned. A zero might mean that the bus is shorted, there are no devices,
-  // or you have already retrieved all of them. It might be a good idea to
-  // check the CRC to make sure you didn't get garbage. The order is
-  // deterministic. You will always get the same devices in the same order
-  //
-  // Must be called before search()
-  //oneWire.reset_search();
-  // assigns the first address found to insideThermometer
-  //if (!oneWire.search(insideThermometer)) Serial.println("Unable to find address for insideThermometer");
-  // assigns the seconds address found to outsideThermometer
-  //if (!oneWire.search(outsideThermometer)) Serial.println("Unable to find address for outsideThermometer");
-
-  // show the addresses we found on the bus
-  Serial.print("Device 0 Address: ");
-  printAddress(tfreezer);
+  Serial.print("inx0 0 Address: ");
+  printAddress(inx0);
+  Serial.println();
+  
+  Serial.print("inx1 0 Address: ");
+  printAddress(inx1);
   Serial.println();
 
-  //Serial.print("Device 1 Address: ");
-  //printAddress(outsideThermometer);
-  //Serial.println();
+  Serial.print("Ambient sensor address: ");
+  printAddress(devambient);
+  Serial.println();
+
+  Serial.print("Freezer sensor address: ");
+  printAddress(devfreezer);
+  Serial.println();
 
   // set the resolution to 9 bit per device
   
-  sensors.setResolution(tfreezer, TEMPERATURE_PRECISION);
-  //sensors.setResolution(outsideThermometer, TEMPERATURE_PRECISION);
+  sensors.setResolution(devambient, TEMPERATURE_PRECISION);
+  sensors.setResolution(devfreezer, TEMPERATURE_PRECISION);
 
-  Serial.print("Device 0 Resolution: ");
-  Serial.print(sensors.getResolution(tfreezer), DEC);
+  Serial.print("Device ambient Resolution: ");
+  Serial.print(sensors.getResolution(devambient), DEC);
   Serial.println();
 
-  //Serial.print("Device 1 Resolution: ");
-  //Serial.print(sensors.getResolution(outsideThermometer), DEC);
-  //Serial.println();
+  Serial.print("Device freezer Resolution: ");
+  Serial.print(sensors.getResolution(devfreezer), DEC);
+  Serial.println();
 
 // builtin LED
   pinMode(LED_BUILTIN,OUTPUT);
   digitalWrite(LED_BUILTIN,HIGH); // turn LED off
 
-  pinMode(16,OUTPUT);
-  digitalWrite(16,LOW);
-
-
-
+  pinMode(ON_LED,OUTPUT);
+  digitalWrite(ON_LED,LOW);
 }
 
 // function to print a device address
-void printAddress(DeviceAddress deviceAddress)
-{
+void printAddress(DeviceAddress deviceAddress) {
   for (uint8_t i = 0; i < 8; i++)
   {
     // zero pad the address if necessary
@@ -299,7 +286,7 @@ void loop() {
                      // 15 seconds the intCnt > 19 is 5 minutes
     
     digitalWrite(LED_BUILTIN,LOW); // turn LED on
-    // digitalWrite(16,HIGH);  //turn on switch
+    
     gotInt = false;
     intCnt = 0;
 
@@ -310,24 +297,29 @@ void loop() {
     Serial.println("DONE");
 
     // print the device information
-    printData(tfreezer);
+    printData(devambient);
+    printData(devfreezer);
+
     //printData(outsideThermometer);
 
-    float tempC = sensors.getTempC(tfreezer);
+    float tempC = sensors.getTempC(devfreezer);
     
     if (state and (DallasTemperature::toFahrenheit(tempC) < low_off)) {
-      digitalWrite(16, LOW);
+      digitalWrite(ON_LED, LOW);
       state = false;
       }
     else if (!state and (DallasTemperature::toFahrenheit(tempC) > high_on)) {
-      digitalWrite(16, HIGH);
+      digitalWrite(ON_LED, HIGH);
       state = true;
     }
   
-    ffreezer->save(DallasTemperature::toFahrenheit(tempC));
+    feedfreezer->save(DallasTemperature::toFahrenheit(tempC));
 
+    tempC = sensors.getTempC(devambient);
+    feedambient->save(DallasTemperature::toFahrenheit(tempC));
+
+    
     digitalWrite(LED_BUILTIN,HIGH);  // turn LED off
-    //digitalWrite(16,LOW);  // turn switch off
    
   } // end of process timer interupt
 
